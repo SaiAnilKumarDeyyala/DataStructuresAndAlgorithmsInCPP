5da0c51dd821d1b0bcd08675917cc8e14c7db436
Sai Anil Kumar Deyyala <71390941+SaiAnilKumarDeyyala@users.noreply.github.com>
2022-01-17 21:58:43 +0530

    Initial Commit

--- Binary Search Tree/PreOrderTraversal.cpp
+++ Binary Search Tree/PreOrderTraversal.cpp
@@ -6,3 +6,27 @@ void PreOrderTraversal(BSTNode* root) {
     PreOrderTraversal(root->left);
     PreOrderTraversal(root->right);
 }
+
+
+// PreOrder Traversal Using Iterative Method
+vector<int> preorderTraversal(BSTNode* root) {
+        vector<int> preorder;    
+        if(root==NULL) return preorder;   
+
+        stack<TreeNode*> st;        // stack to store the address of the nodes
+        st.push(root);              
+        
+        while(!st.empty()) {       // while stack is not empty
+            root = st.top();        
+            st.pop();
+            preorder.push_back(root->val);
+            
+            if(root->right!=NULL) {        // pushing the right child of the node in the stack
+                st.push(root->right);
+            }
+            if(root->left != NULL) {      // pushing the left child of the node in the stack
+                st.push(root->left);
+            }
+        }
+    return preorder;
+}
