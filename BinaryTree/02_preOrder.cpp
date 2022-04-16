84c882cbb123248df9d4e768a6eeb73c7a57f113
Sai Anil Kumar Deyyala <71390941+SaiAnilKumarDeyyala@users.noreply.github.com>
2022-04-16 10:07:44 +0530

    Update 02_preOrder.cpp

--- BinaryTree/02_preOrder.cpp
+++ BinaryTree/02_preOrder.cpp
@@ -1,5 +1,22 @@
 // PreOrder Traversal Of Binary Tree
 
+// using Recursion
+
+void preorderr(TreeNode* root,vector<int> &vctr) {
+        if(root==NULL) return;
+        vctr.push_back(root->val);
+        if(root->left) preorderr(root->left,vctr);
+        if(root->right) preorderr(root->right,vctr);
+    
+}
+vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        preorderr(root,preorder);
+        return preorder;
+}
+
+
+// using iterative method
 vector<int> preorderTraversal(TreeNode* root) {
   vector<int> preorder;
   if(root==NULL) return preorder;
