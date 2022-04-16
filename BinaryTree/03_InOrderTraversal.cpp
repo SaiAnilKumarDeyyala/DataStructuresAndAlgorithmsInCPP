84f6905af39aa00f935f7ef093c4a05ec25476eb
Sai Anil Kumar Deyyala <71390941+SaiAnilKumarDeyyala@users.noreply.github.com>
2022-04-16 10:35:36 +0530

    Update 03_InOrderTraversal.cpp

--- BinaryTree/03_InOrderTraversal.cpp
+++ BinaryTree/03_InOrderTraversal.cpp
@@ -1,5 +1,22 @@
 // inorder Traversal Of Binary Tree
 
+// recursive method
+void inorder(TreeNode* root,vector<int> &v) {
+        if(root==NULL) return;
+        if(root->left) inorder(root->left,v);
+        v.push_back(root->val);
+        if(root->right) inorder(root->right,v);
+}
+vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> iorder;
+        if(root==NULL) return iorder;
+        inorder(root,iorder);
+        return iorder;
+ }
+
+
+// Iterative method
+
 vector<int> inorderTraversal(TreeNode* root) {
   vector<int> inorder;
   if(root==NULL) return inorder;
